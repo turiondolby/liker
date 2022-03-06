45c81669017260ae04cd94679cc6e43ef77766a3
Haleem Hosein <haleemhosein@yahoo.com>
2022-03-06 16:11:34 -0400

    04 Listing posts -- pass posts to livewire component and style view

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,3 +1,19 @@
 <div>
-    posts
+    @foreach($posts as $post)
+        <div class="flex items-center justify-between border-b border-gray-200 p-3">
+            <div class="flex">
+                <img src="..." alt="..." class="w-12 h-12 rounded-full mr-4">
+                <div>
+                    <h2 class="font-semibold">
+                        {{ $post->user->name }}
+                        <span class="text-sm text-gray-500">{{ $post->created_at->diffForHumans() }}</span>
+                    </h2>
+                    <p class="mb-2">{{ $post->body }}</p>
+                </div>
+            </div>
+            <div>
+                Likes
+            </div>
+        </div>
+    @endforeach
 </div>
