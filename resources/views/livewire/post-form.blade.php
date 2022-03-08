d748f2dbdcc5f28917f5b4ab1640b0d3460b8494
Haleem Hosein <haleemhosein@yahoo.com>
2022-03-07 20:07:13 -0400

    07 Submitting and handling validation -- wire:submit and wire:model

--- resources/views/livewire/post-form.blade.php
+++ resources/views/livewire/post-form.blade.php
@@ -1,9 +1,14 @@
 <div>
-    <form action="#">
+    <form wire:submit.prevent="storePost">
         <div class="mb-3">
             <label for="body" class="sr-only">Post body</label>
-            <textarea name="body" id="body" cols="30" rows="3" class="w-full border-2 border-gray-200 rounded-lg"
+            <textarea wire:model.defer="body" id="body" cols="30" rows="3"
+                      class="w-full border-2 border-gray-200 rounded-lg @error('body') border-red-500 @enderror"
                       placeholder="Share something..."></textarea>
+
+            @error('body')
+            <span class="font-semibold text-red-500 text-sm">{{ $message }}</span>
+            @enderror
         </div>
 
         <button type="submit" class="h-10 px-4 text-center text-white bg-indigo-500 rounded-lg font-medium font-semibold">Post</button>
