09c36388a5a5d7e1c60f0fc49eb991b81278ee89
Haleem Hosein <haleemhosein@yahoo.com>
2022-03-09 20:00:07 -0400

    12 Setting up likes

---
+++ database/migrations/2022_03_09_195159_create_likes_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
